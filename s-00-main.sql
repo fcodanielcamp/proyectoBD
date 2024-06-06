8914d7cac763e6b446cbeb1b1ded6e7bd2b198f3
fcodanielcamp <fcodanielcamp@gmail.com>
2024-06-06 15:52:51 -0600

    Agrego s-05-secuencias.sql con su correspondiente modificación en s-00-main.sql. Pequeña corrección en comentarios de s-08-vistas.sql.

--- s-00-main.sql
+++ s-00-main.sql
@@ -2,7 +2,7 @@
 @s-02-entidades.sql
 --@s-03-tablas-temporales.sql
 --@s-04-tablas-externas.sql
---@s-05-secuencias.sql
+@s-05-secuencias.sql
 @s-06-indices.sql
 --@s-07-sinonimos.sql
 @s-08-vistas.sql
