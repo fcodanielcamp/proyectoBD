e63ee64fbfde9f5929d7735f5546f3cf440acac9
fcodanielcamp <fcodanielcamp@gmail.com>
2024-06-09 11:17:57 -0600

    Corrigiendo indentación s-13-p-crea_actualiza_pedido.sql y s-14-p-crea_actualiza_pedido-prueba.sql.

--- s-14-p-crea_actualiza_pedido-prueba.sql
+++ s-14-p-crea_actualiza_pedido-prueba.sql
@@ -12,21 +12,17 @@ variable v_pedido_id number
 exec dbms_output.put_line('Prueba para la creacion de un nuevo pedido sin asignar un id');
 -- Ejecutamos el procedimiento que realizamos 
 declare
-    begin
-        crea_actualiza_pedido(:v_pedido_id, sysdate, 100.50, 44, 68, 1, null);
-    commit; -- Todo se realizó de forma correcta
-    
+begin
+  crea_actualiza_pedido(:v_pedido_id, sysdate, 100.50, 44, 68, 1, null);
+  commit; -- Todo se realizó de forma correcta
 exception
-    when others then
-    -- Algo salió mal por lo que aplicamos rollback
-    rollback;
+  when others then
+  -- Algo salió mal por lo que aplicamos rollback
+  rollback;
 end;
 /
 -- mostrar el id del pedido insertado
 exec dbms_output.put_line('Nuevo pedido creado/actualizado con el id: ' || :v_pedido_id);
-
-
-
 
 -- Segunda prueba
 exec dbms_output.put_line('Prueba para la actualizacion de un pedido existente con un id');
@@ -34,14 +30,16 @@ exec :v_pedido_id := 6;
 
 -- Ejecutamos el procedimiento que realizamos 
 begin
-    crea_actualiza_pedido(:v_pedido_id, v_estado_actual => 2);
-    commit; -- todo se realizó de forma correcta
-
+  crea_actualiza_pedido(:v_pedido_id, v_estado_actual => 2);
+  commit; -- todo se realizó de forma correcta
 exception
-    when others then
-        -- algo salió mal por lo que aplicamos rollback
-        rollback;
+  when others then
+    -- algo salió mal por lo que aplicamos rollback
+    rollback;
 end;
 /
 -- mostrar el id del pedido actualizado
 exec dbms_output.put_line('Se actualizo el pedido con id: ' || :v_pedido_id);
+
+prompt Haciendo rollback para limpiar los datos de Prueba
+rollback;
